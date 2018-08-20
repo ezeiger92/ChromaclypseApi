a6fe07434d4fb896e8b34c0b3bef5fdec5136cbe
BuildTools <unconfigured@null.spigotmc.org>
2018-08-20 14:05:31 -0700

    Clicky

---
+++ src/main/java/com/chromaclypse/api/menu/Clicks.java
@@ -0,0 +1,30 @@
+package com.chromaclypse.api.menu;
+
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class Clicks {
+	private Clicks() {
+	}
+
+	public static int number(InventoryClickEvent event, int amount, int shiftModifier) {
+		switch(event.getClick()) {
+			case NUMBER_KEY: {
+				return amount * 10 + event.getHotbarButton();
+			}
+
+			case DROP: {
+				return amount / 10;
+			}
+
+			case CONTROL_DROP: {
+				return 0;
+			}
+
+			default: {
+				int increment = (event.isRightClick() ? -1 : 1) * (event.isShiftClick() ? shiftModifier : 1);
+	
+				return amount + increment;
+			}
+		}
+	}
+}
