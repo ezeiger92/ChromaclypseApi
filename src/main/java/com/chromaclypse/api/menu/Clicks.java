f6964a8612fee956fb6934abe0f7d7fc38851199
BuildTools <unconfigured@null.spigotmc.org>
2018-08-23 18:50:19 -0700

    int and long

--- src/main/java/com/chromaclypse/api/menu/Clicks.java
+++ src/main/java/com/chromaclypse/api/menu/Clicks.java
@@ -6,7 +6,7 @@ public class Clicks {
 	private Clicks() {
 	}
 
-	public static int number(InventoryClickEvent event, int amount, int shiftModifier) {
+	public static long number(InventoryClickEvent event, long amount, long shiftModifier) {
 		switch(event.getClick()) {
 			case NUMBER_KEY: {
 				return amount * 10 + event.getHotbarButton() + 1;
@@ -21,10 +21,14 @@ public class Clicks {
 			}
 
 			default: {
-				int increment = (event.isRightClick() ? -1 : 1) * (event.isShiftClick() ? shiftModifier : 1);
+				long increment = (event.isRightClick() ? -1 : 1) * (event.isShiftClick() ? shiftModifier : 1);
 	
 				return amount + increment;
 			}
 		}
+	}
+
+	public static int number(InventoryClickEvent event, int amount, int shiftModifier) {
+		return (int) number(event, (long) amount, (long) shiftModifier);
 	}
 }
